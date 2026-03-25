44982f951e273b1072df53c155d6199b5d009e91
digaus <di.gaus@gmx.de>
2026-03-25 22:38:57 +0100

    Update __init__.py

--- components/bthome_receiver_base/__init__.py
+++ components/bthome_receiver_base/__init__.py
@@ -471,12 +471,12 @@ class Generator:
                         var_item.set_device_class(
                             config_item[CONF_DEVICE_CLASS]
                         )
-                     )
-                     cg.add(
-                         var_item.set_unit_of_measurement(
-                             config_item[CONF_UNIT_OF_MEASUREMENT]
-                         )
-                     )
+                    )
+                    cg.add(
+                        var_item.set_unit_of_measurement(
+                            config_item[CONF_UNIT_OF_MEASUREMENT]
+                        )
+                    )
                 # last statement - add the sensor
                 cg.add(
                     paren.add_sensor(
